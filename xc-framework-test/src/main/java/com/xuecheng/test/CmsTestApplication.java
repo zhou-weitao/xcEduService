e8d584ecdf3a4ebdc0672c19e60b6ac2828e4442
zhouweitao_ext <E/JErkOjTrI0O/7cU0SxhsmTvrXX9GsDULsnsfza8g>
2020-05-20 22:24:30 +0800

    Initial commit

---
+++ xc-framework-test/src/main/java/com/xuecheng/test/CmsTestApplication.java
@@ -0,0 +1,23 @@
+package com.xuecheng.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author wtzhou
+ * @date 2020/5/20 5:04 下午
+ * 扫描实体类
+ * 扫描接口
+ * 扫描本项目下的所有类
+ **/
+@SpringBootApplication
+@EntityScan("com.xuecheng.framework.domain.cms")
+@ComponentScan(basePackages={"com.xuecheng.api"})
+@ComponentScan(basePackages={"com.xuecheng.test"})
+public class CmsTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CmsTestApplication.class, args);
+    }
+}
---
+++ xc-framework-test/src/test/java/com/xuecheng/test/MongoTest.java
@@ -0,0 +1,22 @@
+package com.xuecheng.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author wtzhou
+ * @date 2020/5/20 5:15 下午
+ **/
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MongoTest {
+
+   @Test
+   public void testRepository() {
+       System.out.println("123421");
+   }
+
+
+}
