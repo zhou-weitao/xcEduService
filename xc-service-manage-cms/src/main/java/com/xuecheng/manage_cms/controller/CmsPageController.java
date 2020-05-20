e74e20d63ec4fb345be23da9f5c521bf8b343793
zhouweitao_ext <E/JErkOjTrI0O/7cU0SxhsmTvrXX9GsDULsnsfza8g>
2020-05-20 22:36:04 +0800

    测试分支

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
@@ -42,7 +42,6 @@ public class CmsPageController implements CmsPageControllerApi {
 
         QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS,queryResult);
         return queryResponseResult;*/
-        //调用service
         return pageService.findList(page,size,queryPageRequest);
     }
 }
